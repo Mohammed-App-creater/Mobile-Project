978e10463cdb598cbe021eee9ccc6063b85674c2
Gadisa Yusuf Aliyi <gadisa672@gmail.com>
2025-05-13 21:25:10 +0300

    Setup dotenv for middleware

---
+++ backend/middlewares/jwtValidator.js
@@ -0,0 +1,7 @@
+import dotenv from 'dotenv';
+dotenv.config(); // Load environment variables from .env file
+
+// Replace this with your actual secret or use process.env.JWT_SECRET
+const JWT_SECRET = process.env.JWT_SECRET ;
+
+
