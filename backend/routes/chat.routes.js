e318627be5591bbeba4a5a6bb3851d080ea615ae
hawi3075 <hawig3266@gmail.com>
2025-05-16 09:04:53 +0300

    added history routes

--- backend/routes/chat.routes.js
+++ backend/routes/chat.routes.js
@@ -1,12 +1,12 @@
 import express from "express";
 import { sendPrompt } from "../controllers/chatController.js";
 import jwtValidator  from "../middlewares/jwtValidator.js";
-
+import { getAllUserChatHistory } from "../controllers/getAllUserChatHistory.js";
 
 const router = express.Router();
 
 
 router.post("/send-prompt", jwtValidator, sendPrompt);
-
+router.get('/history', jwtValidator, getAllUserChatHistory);
 
 export default router;
