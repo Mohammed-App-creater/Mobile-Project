88d28cd2043938aac30e9a42a3e10738f222f9c1
hawi3075 <hawig3266@gmail.com>
2025-05-17 16:57:05 +0300

    add data base and route imports

--- backend/index.js
+++ backend/index.js
@@ -3,3 +3,8 @@ import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+import authRoutes from './routes/auth.js';
+import chatRoutes from './routes/chat.routes.js'; 
+
+
